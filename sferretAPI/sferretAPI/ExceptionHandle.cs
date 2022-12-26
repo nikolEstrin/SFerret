a57908be174629d496ff433d0fef7d2d253eb65f
MatanShn <102690877+MatanShn@users.noreply.github.com>
2022-12-26 14:44:54 +0200

    SFerret RestApi Server

---
+++ sferretAPI/sferretAPI/ExceptionHandle.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json;
+using System.Net;
+
+namespace sferretAPI
+{
+    public class ExceptionHandle
+    {
+        private readonly RequestDelegate requestDelegate;
+        public ExceptionHandle(RequestDelegate requestDelegate)
+        {
+            this.requestDelegate = requestDelegate;
+        }
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await requestDelegate(context);
+            }
+            catch (Exception ex)
+            {
+                await HandleException(context, ex);
+            }
+        }
+        
+        private async Task HandleException(HttpContext context, Exception ex)
+        {
+            
+            var errorMessage = JsonConvert.SerializeObject(new { Message = ex.Message, Code = "GE" });
+
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+            await context.Response.WriteAsync(errorMessage);
+            //return context.Response.WriteAsync(errorMessage);
+        }
+    }
+}
---
+++ sferretAPI/sferretAPI/Models/Post.cs
@@ -0,0 +1,12 @@
+﻿namespace sferretAPI.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int MovieId { get; set; }
+        public int Rating { get; set; }
+        public string Comment { get; set; }
+        public DateTime PublishedDate { get; set; }
+    }
+}
