1d7c968f39c96f9de94a8588f73719aa1aa32164
MatanShn <matan.shneider@live.biu.ac.il>
2023-01-09 23:52:10 +0200

    pagination in movie's service

--- sferretAPI/sferretAPI/Controllers/MovieController.cs
+++ sferretAPI/sferretAPI/Controllers/MovieController.cs
@@ -29,65 +29,73 @@ namespace sferretAPI.Controllers
 
         [Route("/Movies")]
         [HttpGet]
-        public async Task<IActionResult> GetAll()
+        public async Task<IActionResult> GetAll(int page)
         {
-            var movie = await _movieService.GetAll();
+            var movie = await _movieService.GetAll(page);
+            return Ok(movie);
+        }
+
+        [Route("/Movies/{id}")]
+        [HttpGet]
+        public async Task<IActionResult> GetAllNotWatched(int id, int page)
+        {
+            var movie = await _movieService.GetAllNotWatched(id, page);
             return Ok(movie);
         }
 
         [HttpGet("Collection/{collection}")]
-        public async Task<IActionResult> GetByCollection(string collection)
+        public async Task<IActionResult> GetByCollection(string collection, int page)
         {
-            var list = await _movieService.GetByCollection(collection);
+            var list = await _movieService.GetByCollection(collection, page);
             return Ok(list);
         }
 
         [HttpGet("Lang/{lang}")]
-        public async Task<IActionResult> GetByLanguage(string lang)
+        public async Task<IActionResult> GetByLanguage(string lang, int page)
         {
-            var list = await _movieService.GetByLanguage(lang);
+            var list = await _movieService.GetByLanguage(lang, page);
             return Ok(list);
         }
 
         [HttpGet("Title/{title}")]
-        public async Task<IActionResult> GetByTitle(string title)
+        public async Task<IActionResult> GetByTitle(string title, int page)
         {
-            var list = await _movieService.GetByTitle(title);
+            var list = await _movieService.GetByTitle(title, page);
             return Ok(list);
         }
 
         [HttpGet("Date/{date}/Flag/{flag}")]
-        public async Task<IActionResult> GetByDate(DateTime date, int flag)
+        public async Task<IActionResult> GetByDate(DateTime date, int flag, int page)
         {
-            var list = await _movieService.GetByDate(date, flag);
+            var list = await _movieService.GetByDate(date, flag, page);
             return Ok(list);
         }
 
         [HttpGet("Time/{runtime}/Flag/{flag}")]
-        public async Task<IActionResult> GetByRuntime(int runtime, int flag)
+        public async Task<IActionResult> GetByRuntime(int runtime, int flag, int page)
         {
-            var list = await _movieService.GetByRuntime(runtime, flag);
+            var list = await _movieService.GetByRuntime(runtime, flag, page);
             return Ok(list);
         }
 
         [HttpGet("Genre/{genre}")]
-        public async Task<IActionResult> GetByGenre(string genre)
+        public async Task<IActionResult> GetByGenre(string genre, int page)
         {
-            var list = await _movieService.GetByGenre(genre);
+            var list = await _movieService.GetByGenre(genre, page);
             return Ok(list);
         }
 
         [HttpGet("Rating/{rating}/Flag/{flag}")]
-        public async Task<IActionResult> GetByRating(int rating, int flag)
+        public async Task<IActionResult> GetByRating(int rating, int flag, int page)
         {
-            var list = await _movieService.GetByRating(rating, flag);
+            var list = await _movieService.GetByRating(rating, flag, page);
             return Ok(list);
         }
 
         [HttpGet("Post/{post}")]
-        public async Task<IActionResult> GetByPost(bool post)
+        public async Task<IActionResult> GetByPost(bool post, int page)
         {
-            var movie = await _movieService.GetByPost(post);
+            var movie = await _movieService.GetByPost(post, page);
             return Ok(movie);
         }
     }
